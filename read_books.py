638e4cf29bc2726be71f0dc9319c45d66ffef1fa
Trey Hernandez <Tahrnndz@gmail.com>
2022-03-13 08:24:21 -0500

    able to write all card data to excel spreadsheet now

--- read_books.py
+++ read_books.py
@@ -52,7 +52,7 @@ for i, value in enumerate(json_list):
     status_col = "D"+str(column)
     sheet[name_col] = name
     sheet[start_col] = start_date
-    sheet[dnf_col] = start_date
+    sheet[dnf_col] = dnf_date
     sheet[status_col] = "Did Not Finish"
     wb.save(target_file)   
 
@@ -69,19 +69,33 @@ response = requests.request(
    url,
    headers=headers
 )
-
+print('Column: ' + str(column))
 json_list = json.loads(response.text)
 print('Finished!')
 for i, value in enumerate(json_list):
+    column += 1
     name = json_list[i]["name"] 
-    print(name)
+   #  print(name)
     description = json_list[i]["desc"].strip().replace('\n',"")
-    print(description)
+   #  print(description)
 
     start_date = description[9:19].strip()
-    print("start_date: " + start_date)
+   #  print("start_date: " + start_date)
 
     completed_date = description[description.find('Com')+11:].strip()
-    print("completed_date: " + completed_date)
+   #  print("completed_date: " + completed_date)
 
-    print()
+   #  print()
+    wb = openpyxl.load_workbook(target_file)
+    sheet = wb.active
+    name_col = "A"+str(column)
+    start_col = "B"+str(column)
+    completed_col = "C"+str(column)
+    status_col = "D"+str(column)
+    sheet[name_col] = name
+    sheet[start_col] = start_date
+    sheet[completed_col] = completed_date
+    sheet[status_col] = "Completed"
+    wb.save(target_file)   
+
+wb.save(target_file)    
