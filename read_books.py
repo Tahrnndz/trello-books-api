2525fadbc2f61103b2fd1aa353cca6a22bb0b3aa
Trey Hernandez <Tahrnndz@gmail.com>
2022-03-13 08:16:53 -0500

    added excel writer to first loop

--- read_books.py
+++ read_books.py
@@ -3,6 +3,12 @@
 import requests
 import json
 from decouple import config
+
+import openpyxl, csv
+# define filename and file path of source and target files
+# source_file = '/Users/Admin/OneDrive/0000.Journals/0000.DailyJournalArchive.txt'
+target_file = '/Users/Admin/test.xlsx'
+
 API_TOKEN = config('TOKEN')
 API_KEY = config('KEY')
 DNF_LIST_ID = config('DNF_LIST_ID')
@@ -22,18 +28,36 @@ response = requests.request(
 json_list = json.loads(response.text)
 print()
 for i, value in enumerate(json_list):
+    column = i + 1
+   #  print(i)
+   #  print(column)
     name = json_list[i]["name"] 
-    print(name)
+   #  print(name)
     description = json_list[i]["desc"].strip().replace('\n',"")
-    print(description)
+   #  print(description)
 
     start_date = description[9:19].strip()
-    print("start_date: " + start_date)
+   #  print("start_date: " + start_date)
 
     dnf_date = description[description.find('DNF')+4:].strip()
-    print("dnf_date: " + dnf_date)
+   #  print("dnf_date: " + dnf_date)
 
-    print()
+   #  print()
+
+    wb = openpyxl.load_workbook(target_file)
+    sheet = wb.active
+    name_col = "A"+str(column)
+    start_col = "B"+str(column)
+    dnf_col = "C"+str(column)
+    status_col = "D"+str(column)
+    sheet[name_col] = name
+    sheet[start_col] = start_date
+    sheet[dnf_col] = start_date
+    sheet[status_col] = "Did Not Finish"
+    wb.save(target_file)   
+
+wb.save(target_file)    
+
 
 url = f"https://api.trello.com/1/lists/{COMPLETED_LIST_ID}/cards?key={API_KEY}&token={API_TOKEN}"
 headers = {
